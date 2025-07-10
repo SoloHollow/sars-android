809e0b5f39caea79263b4054c71b23d0c4a544cb
Swassyman <swassyman2005@gmail.com>
2025-07-10 23:48:52 +0530

    saving files before merge

--- app/src/main/java/com/example/sars/LoginScreen.kt
+++ app/src/main/java/com/example/sars/LoginScreen.kt
@@ -3,7 +3,6 @@ package com.example.sars
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
